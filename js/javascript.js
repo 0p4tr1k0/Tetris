fc7f43a529c684abfc5ce3c3cc31316791373709
0p4tr1k0 <BeruJenHotovostZRukyDoRuky@seznam.cz>
2021-01-19 18:29:59 +0100

    mazani plneho radku

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,24 @@
+/*Canvas samotný*/
 const canvas =  document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
-
+/*Zvětšení*/
 context.scale(20, 20);
 
+function arenaSweep() {
+    outer: for(let y = arena.length - 1; y > 0; --y){
+        for(let x = 0; x < arena[y].length; ++x){
+            if(arena[y][x] === 0){
+                continue outer;
+            }
+        }
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+    }
+}
 
-
+/*Funkce, která vytváří "stěny" na krajích canvasu*/
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
     for (let y = 0; y < m.length; ++y){
@@ -19,7 +32,7 @@ function collide(arena, player) {
     }
     return false;
 }
-
+/*Funkce, která vytváří hrací plochu.*/
 function createMatrix(w, h){
     const matrix = [];
     while (h--) {
@@ -27,7 +40,7 @@ function createMatrix(w, h){
     }
     return matrix;
 }
-
+/*Funkce, která vytváří kousky.*/
 function createPiece(type){
     if(type === 'T'){
         return [
@@ -38,48 +51,48 @@ function createPiece(type){
     }
     else if(type === 'O'){
         return [
-            [1, 1],
-            [1, 1],
+            [2, 2],
+            [2, 2],
         ]
     }
     else if(type === 'L'){
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1],
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
         ]
     }
     else if(type === 'J'){
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0],
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
         ]
     }
     else if(type === 'I'){
         return [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
         ]
     }
     else if(type === 'S'){
         return [
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0],
         ]
     }
     else if(type === 'Z'){
         return [
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0],
         ]
     }
 }
-
+/*Funkce, která umožňuje vykreslovat na canvas*/
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
@@ -87,18 +100,18 @@ function draw() {
     drawMatrix(arena, {x: 0, y: 0});
     drawMatrix(player.matrix, player.pos);
 }
-
+/*Funkce pro zobrazení kousku na canvasu*/
 function drawMatrix(matrix, offset){
     matrix.forEach((row, y) =>{
         row.forEach((value, x) =>{
             if(value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
     });
 }
-
+/*Funkce, která spojuje kousky (aby se nemohly prolínat).*/
 function merge(arena, player) {
     player.matrix.forEach((row, y) => {
         row.forEach((value, x) => {
@@ -109,31 +122,35 @@ function merge(arena, player) {
 });
 
 }
-
+/*Funkce, která umožňuje při pádu zastavit kousek, buď o další kousek, nebo o spodek canvasu */
 function playerDrop(){
     player.pos.y++;
     if(collide(arena, player)){
         player.pos.y--;
         merge(arena, player);
         playerReset();
+        arenaSweep();
     }
     dropCounter = 0;
 }
-
+/*Funkce pro pohyb a kolize */
 function playerMove(dir){
     player.pos.x += dir;
     if(collide(arena, player)){
         player.pos.x -= dir;
     }
 }
-
+/*Funkce, která náhodně vybírá kousky + vyresetuje, když se na canvasu kousek dotkne vrchu */
 function playerReset(){
     const pieces = 'ILJOTSZ';
     player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
     player.pos.y = 0;
     player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
+    if(collide(arena, player)){
+        arena.forEach(row => row.fill(0));
+    }
 }
-
+/*Funkce, která umožní hráči otáčet kouskem tak, aby se nemohl zaseknout na kraji canvasu(kousek zpátky vyskočí do canvasu)*/
 function playerRotate(dir){
     const pos = player.pos.x;
     let offset = 1;
@@ -148,7 +165,7 @@ function playerRotate(dir){
         }
     }
 }
-
+/*Funkce, která dokáže otáčet kousky*/
 function rotate(matrix, dir){
     for(let y = 0; y < matrix.length; ++y) {
         for (let x = 0; x < y; ++x){
@@ -162,14 +179,13 @@ function rotate(matrix, dir){
         matrix.reverse();
     }
 }
-
+/*funkce, která updatuje canvas a umožňuje pohybovat kousky (co sekundu se pohne sám dolů)*/
 let dropCounter = 0;
 let dropInterval = 1000;
 let LastTime = 0;
 function update(time = 0) {
     const deltaTime = time - LastTime;
     LastTime = time;
-    
     dropCounter += deltaTime;
     if (dropCounter > dropInterval) {
         playerDrop();
@@ -178,12 +194,30 @@ function update(time = 0) {
     requestAnimationFrame(update);
 }
 
-const arena = createMatrix(12, 20);
+function updateScore(){
+    document.getElementById('score').innerHTML = player.score;
+}
 
+/*barvy kousků*/
+const colors = [
+    null,
+    '#FF0D72',
+    '#0DC2FF',
+    '#0DFF72',
+    '#F538FF',
+    '#FF8E0D',
+    '#FFE138',
+    '#3877FF',
+
+];
+/*funkce pro canvas, která určuje maximální počet kousků v poli (kostiček)*/
+const arena = createMatrix(12, 20);
+/*funkce, kde začíná padat kousek*/
 const player = {
     pos: {x:5, y:5},
     matrix: createPiece('T'),
 }
+/*Funkce pro ovladání kousků (jednoduše ovládání)*/
 document.addEventListener('keydown', event =>{
     if (event.keyCode === 37 || event.keyCode === 65){
         playerMove(-1);
